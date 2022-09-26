3671d1bcaa7fb0bfbecb9a38c6f16fb69d779c4b
alex-main-academy <oleksandrilnytskyi@gmail.com>
2022-09-26 08:29:26 +0200

    Merge branch 'main' into reviews

---
+++ src/slider.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
